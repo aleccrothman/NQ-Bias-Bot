8047b59d060730e58a708b423f546b4c4665963c
aleccrothman <aleccrothman@gmail.com>
2026-03-24 17:16:27 -0400

    Update nq_bias_bot.py

--- nq_bias_bot.py
+++ nq_bias_bot.py
@@ -826,9 +826,9 @@ def main():
 
     # Uncomment to test immediately
     # run_news_job()
-    run_morning_bias()
-    run_nyo_update()
-    run_eod_score()
+    # run_morning_bias()
+    # run_nyo_update()
+    # run_eod_score()
 
     while True:
         schedule.run_pending()
